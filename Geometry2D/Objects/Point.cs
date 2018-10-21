58b985b8bfcdf7af5806bc3ad7bc09bca15172ff
yz_in_aeternum <yuri.zakharyan@gmail.com>
2018-10-21 15:13:12 +0300

    Point.cs: regions added

--- Geometry2D/Objects/Point.cs
+++ Geometry2D/Objects/Point.cs
@@ -7,16 +7,16 @@ namespace Geometry2D.Objects
     {
         private double _x;
         private double _y;
+
+        #region Constructors
         public Point(double x, double y)
         {
             _x = x;
             _y = y;
         }
-        public Vector ToVector() => new Vector(this);
-        public double X { get => _x; }
-        public double Y { get => _y; }
+        #endregion
 
-        
+        #region Comparasion
         public override bool Equals(object obj)
         {
             if (obj == null) return false;
@@ -24,21 +24,44 @@ namespace Geometry2D.Objects
             if ((object)temp == null) return false;
             return Destination(this, temp) < Constants.EPS;
         }
+
         public override int GetHashCode()
         {
             return _x.GetHashCode() ^ _y.GetHashCode();
         }
+
         public static bool operator ==(Point point1, Point point2)
         {
             return point1.Equals(point2);
         }
+
         public static bool operator !=(Point point1, Point point2)
         {
             return !point1.Equals(point2);
         }
+        #endregion
+
+        #region Properties
+        public double X { get => _x; }
+
+        public double Y { get => _y; }
+        #endregion
+
+        #region Non-static functions
+        public Vector ToVector() => new Vector(this);
+        #endregion
+
+        #region Static functions
         public static double Destination(Point point1, Point point2) =>
             Math.Sqrt((point2._x - point1._x) * (point2._x - point1._x) + (point2._y - point1._y) * (point2._y - point1._y));
+
         public static Point CoordinateStartPoint() => new Point(0, 0);
+        #endregion
+
+
+
+
+
     }
 
 }
